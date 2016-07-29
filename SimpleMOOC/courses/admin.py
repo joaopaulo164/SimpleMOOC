d313ceb1f6e2e6f230df719b94c498912ed9e01b
Palotec <joaopaulo164@hotmail.com>
2016-07-29 09:42:28 -0300

    Announcements Model

--- SimpleMOOC/courses/admin.py
+++ SimpleMOOC/courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Enrollment, Announcement, Comment
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ class CourseAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Course, CourseAdmin)
+admin.site.register([Enrollment, Announcement, Comment])
--- SimpleMOOC/courses/models.py
+++ SimpleMOOC/courses/models.py
@@ -78,3 +78,53 @@ class Enrollment(models.Model):
 
         # unique_together => index de unicidade, ou seja, só pode existir uma inscrição para cada usuário e curso
         unique_together = (('user', 'course'),)
+
+
+
+class Announcement(models.Model):
+
+    course = models.ForeignKey(Course, verbose_name='Curso')
+    title = models.CharField('Título', max_length=100)
+    content = models.TextField('Conteúdo')
+
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = 'Anúncio'
+        verbose_name_plural = 'Anúncios'
+        ordering = ['-created_at'] # -created_at => decrescente
+
+
+
+class Comment(models.Model):
+
+    announcement = models.ForeignKey(
+        Announcement,
+        verbose_name='Anúncio',
+        related_name='comments' # uma instância de Announcement terá uma relação comments que trará os seus comentários
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Usúario')
+    comment = models.TextField('Comentário')
+
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+
+    class Meta:
+        verbose_name = 'Comentário'
+        verbose_name_plural = 'Comentários'
+        ordering = ['created_at']
+
+
+
+
+
+
+
+
+
